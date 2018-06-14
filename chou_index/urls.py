97480fdfcf4fbbac6363d3f3bf9ad4f90a4136d5
chouchou-mouya <irene101830@hotmail.com>
2018-06-14 17:07:08 +0800

    vue upload

--- chou_index/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    # 進入首頁的意思(因為直接接domain)
-    path("", views.index, name='index'),
-    #path("",views.這裡呼叫def的名稱,nmae="該名稱")
-]
